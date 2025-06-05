9ff90df6c08aff6ce8ebc473b482fa2d06276c6b
Pedro <soporte@peruviandigital.com>
2025-06-04 19:29:17 -0500

    backend connection with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3309,
+    username: 'root',
+    password: '12345',
+    database: 'back_form_app',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
