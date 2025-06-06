37bbb4e24be0f7deb2a4f000c7bae28cdf42bd00
Pedro <soporte@peruviandigital.com>
2025-06-05 19:47:20 -0500

    create controllers with service file

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Put } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('v1/users')
 export class UsersController {
@@ -12,8 +13,27 @@ export class UsersController {
         return this.usersService.findAll();
     }
 
+    @Get('/:id')
+    findOne(@Param('id') id: string): Promise<User> {
+        return this.usersService.findOne(id);
+    }
+
     @Post()
     createUser(@Body() user: CreateUserDto): Promise<User> {
         return this.usersService.createUser(user);
     }
+
+    @Patch('/:id')
+    updateUser(
+        @Param('id') id: string,
+        @Body() userUpdate: UpdateUserDto
+    ): Promise<User> {
+        return this.usersService.updateUser(id, userUpdate)
+    }
+
+    @Put('/:id')
+    deleteUser(@Param('id') id: string) {
+        return this.usersService.deleteUser(id)
+    }
+
 }
