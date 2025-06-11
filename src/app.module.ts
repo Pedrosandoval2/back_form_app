9ebef326fc751e8818a74eea4b10a56ebfc2115b
Pedro <soporte@peruviandigital.com>
2025-06-11 17:43:39 -0500

    Add customerModule file to main

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersModule } from "./users/users.module";
 import { AuthModule } from "./auth/auth.module";
 import { EventsModule } from "./events/events.module";
+import { CustomersModule } from "./customers/customers.module";
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -16,7 +17,7 @@ import { EventsModule } from "./events/events.module";
     database: 'back_form_app',
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
     synchronize: true,
-  }), UsersModule, AuthModule, EventsModule],
+  }), UsersModule, AuthModule, EventsModule, CustomersModule],
   controllers: [AppController],
   providers: [AppService],
 })
